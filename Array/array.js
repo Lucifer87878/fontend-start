b5d2fbd17e01269faf0bf782ae2577b1d32fe260
pribo1 <nallenrockar123@gmail.com>
2023-12-05 10:29:44 +0100

    start av olika projekt

---
+++ Array/array.js
@@ -0,0 +1,38 @@
+// en simpel array 
+
+let fruits = ['kiwi', 'apple', 'pear'];
+
+let animals = ['cat', 'hamster', 'parrot', 'funky chihuahua']
+
+let newAnimal = animals.push("dog");
+
+console.log(newAnimal);
+
+// antal av olika objekt i en array
+console.log(fruits.length); //frukter
+console.log(animals.length); //djur
+
+
+// hämta något specifikt ur arrayen
+console.log(fruits[0]); // Hämta kiwi
+console.log(animals[3]); // Hämta funky chihuahua 
+
+// bytta ut olika värden i en array
+animals[1] = 'tiger'; // Byt ut 'hamster' mot 'tiger'
+fruits[1] = 'Bannan'; // Byt ut 'apple' mot 'Bannan'
+
+
+
+// //------------------------simpla delen-------------------------//
+
+// //------------------------mer anvancerade delen för mig-------------------------//
+// let a = [1, 2, 3];
+// let b = [4, 5, 6];
+// let mergedArray = a.concat(b); // [1, 2, 3, 4, 5, 6]
+
+
+
+
+
+
+
