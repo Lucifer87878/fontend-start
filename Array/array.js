b60251eea9dff734a34f09bf2fbeb5f53a79f4c4
pribo1 <nallenrockar123@gmail.com>
2023-12-05 11:39:23 +0100

    nya  filer

--- Array/array.js
+++ Array/array.js
@@ -17,20 +17,27 @@ console.log(animals.length); //djur
 console.log(fruits[0]); // Hämta kiwi
 console.log(animals[3]); // Hämta funky chihuahua 
 
+console.log(animals[0]);// uppgift 3-5
+
+
 // bytta ut olika värden i en array
 animals[1] = 'tiger'; // Byt ut 'hamster' mot 'tiger'
 fruits[1] = 'Bannan'; // Byt ut 'apple' mot 'Bannan'
 
+console.log(animals[1]); // uppgift 6
 
 
-// //------------------------simpla delen-------------------------//
 
-// //------------------------mer anvancerade delen för mig-------------------------//
-// let a = [1, 2, 3];
-// let b = [4, 5, 6];
-// let mergedArray = a.concat(b); // [1, 2, 3, 4, 5, 6]
+//------------------------simpla delen-------------------------//
 
+//------------------------mer anvancerade delen för mig-------jag behöver öva mer------------------//
+let a = [1, 2, 3];
+let b = [4, 5, 6];
+let mergedArray = a.concat(b); // [1, 2, 3, 4, 5, 6]
 
+console.log(a);
+console.log(b);
+console.log(mergedArray);
 
 
 
